b636902f1dd5c2bee9e96c6ead199ef27bfc05ba
Karl Foley <c3274832@uon.edu.au>
2020-03-30 18:33:41 +1100

    added trigger to stop non students from being assigned to courses

--- Assessment 1/Database/t test_usp_RegisterForCourses_14.sql
+++ Assessment 1/Database/t test_usp_RegisterForCourses_14.sql
@@ -1,14 +1,44 @@
+/*
+Inserts all current course oferrings for a given student number.
+*/
+
 DECLARE @courseOfferingsToRegisterIn AS courseListForStudentType;
 
 INSERT @courseOfferingsToRegisterIn (courseId, timePeriodId, campusId)
    SELECT courseId, timePeriodId, campusId
    From CourseOffering;
 
+--SELECT *
+--FROM @courseOfferingsToRegisterIn
+
+DECLARE @StudentNumber INT
+SET @StudentNumber = 4
+
+EXEC usp_RegisterForCourses @StudentNumber, @courseOfferingsToRegisterIn;
+
+SELECT *
+FROM StudentRegistersInCourse
+
+
+--Only run to clear table - allows for no clashes
+
+DELETE StudentRegistersInCourse
+
+/*
+Inserts a couse that has a precequist that was not been forfilled this will fail and give error messages
+
+*/
+
+DECLARE @courseOfferingsToRegisterIn AS courseListForStudentType;
+
+INSERT INTO @courseOfferingsToRegisterIn (courseId, timePeriodId, campusId)
+VALUES(3,1,1);
+
 SELECT *
 FROM @courseOfferingsToRegisterIn
 
 DECLARE @StudentNumber INT
-SET @StudentNumber = 5
+SET @StudentNumber = 7
 
 EXEC usp_RegisterForCourses @StudentNumber, @courseOfferingsToRegisterIn;
 
