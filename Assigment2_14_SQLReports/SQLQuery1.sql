168afec68e263d6b5a527f03881688d5343f1143
KarlKFoley <42328649+KarlKFoley@users.noreply.github.com>
2020-04-29 13:25:13 +1000

    Section 3a

---
+++ Assigment2_14_SQLReports/SQLQuery1.sql
@@ -0,0 +1,35 @@
+SELECT *
+FROM Dimension.City
+
+SELECT *
+FROM Dimension.City
+WHERE [State Province] = 'Florida'
+
+SELECT *
+FROM Dimension.[Date]
+
+
+SELECT *
+FROM Dimension.Supplier
+
+SELECT *
+FROM Fact.Movement
+
+SELECT *
+FROM Fact.Purchase
+
+SELECT *
+FROM Fact.Sale
+
+SELECT *
+FROM Fact.[Order]
+
+SELECT *
+FROM Fact.[Order]
+WHere [WWI Order ID]=1025
+
+SELECT *
+FROM Fact.[Transaction]
+
+SELECT *
+FROM Fact.[Stock Holding]
